dbb55360c9cee9a8f600fc7dd863553e699d2395
Michal Nazarewicz <mina86@mina86.com>
2012-10-27 17:08:49 +0200

    Added images to the “Itroduction” section

--- find-deps.sh
+++ find-deps.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-sed -ne 's/.*\\includegraphics.*{\(build\/.*\)}.*/images: \1/p' "$@" /dev/null |
+sed -ne 's/.*\\includegraphics.*{\(build\/.*\)}.*/\1/p' "$@" /dev/null |
+	while read img; do
+		echo "images: $img"
+		case "$img" in build/*--*)
+			svg=${img%--*}.svg
+			svg=img/${svg#build/}
+			id=${img##*--}
+			id=${id%.*}
+			echo "$img: $svg"
+			echo '	@mkdir -p build'
+			echo "	inkscape -z -j -i $id --file=\$< --export-eps=\$@"
+		esac
+	done
